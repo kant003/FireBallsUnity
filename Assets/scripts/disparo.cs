b95b5618e0c4d20a89203687432149f199bae844
kant003 <gonzalezm.angel@gmail.com>
2017-12-06 19:23:49 +0100

    escenario bo base, movimiento y salto jugador, disparo de proyectiles

---
+++ Assets/scripts/disparo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparo : MonoBehaviour {
+
+	public Rigidbody bala;
+	public Transform puntoDisparo;
+	public float fuerzaDisparo;
+	public float tiempoEntreDisparos = 0.15f;
+	private float timer;
+
+	//	public float speed = 6.0F;
+	// public float jumpSpeed = 8.0F;
+	// public float gravity = 20.0F;
+	// private Vector3 moveDirection = Vector3.zero;
+
+	CharacterController controller;
+	Rigidbody rigidbody;
+	Transform transform;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+		rigidbody = GetComponent<Rigidbody> ();
+		transform = GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (Input.GetButton ("Fire1") && timer >= tiempoEntreDisparos && Time.timeScale != 0) {
+			timer = 0f;
+			Rigidbody balaInstance = Instantiate (bala, puntoDisparo.position, puntoDisparo.rotation) as Rigidbody;
+
+			balaInstance.velocity = fuerzaDisparo * puntoDisparo.forward;
+
+		}
+
+	}
+
+
+}
---
+++ Assets/scripts/movimiento.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+	private float verticalVelocity;
+	private float gravity = 20.0f;
+	private float jumpForce = 4.0f;
+	public float speed = 6.0F;
+	
+	
+
+	CharacterController controller;
+	Rigidbody rigidbody;
+	Transform transform;
+
+	
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		rigidbody = GetComponent<Rigidbody>();
+		transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(controller.isGrounded){
+			verticalVelocity = -gravity * Time.deltaTime;
+			if(Input.GetKeyDown(KeyCode.Space)){
+				verticalVelocity = jumpForce;
+			}
+		}else{
+			verticalVelocity -=gravity * Time.deltaTime;
+ 
+		}
+		Vector3 moveVector = Vector3.zero;
+		moveVector.x = -Input.GetAxis("Horizontal");
+		moveVector.y = verticalVelocity;
+		moveVector.z = 0;
+		moveVector *= speed;
+
+		controller.Move(moveVector * Time.deltaTime);
+
+
+		         //Mouse Position in the world. It's important to give it some distance from the camera. 
+         //If the screen point is calculated right from the exact position of the camera, then it will
+         //just return the exact same position as the camera, which is no good.
+         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+         
+         //Angle between mouse and this object
+         float angle = AngleBetweenPoints(transform.position, mouseWorldPosition);
+          Debug.DrawLine(transform.position, mouseWorldPosition, Color.red);
+         //Ta daa
+         transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+     }
+     
+     float AngleBetweenPoints(Vector2 a, Vector2 b) {
+         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+     }
+	
+}
