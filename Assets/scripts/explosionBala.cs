2aa58aa0d76f049da25209be78af857d8ef868aa
kant003 <gonzalezm.angel@gmail.com>
2017-12-08 13:43:05 +0100

    Mejora en colisiones bala

---
+++ Assets/scripts/explosionBala.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionBala : MonoBehaviour {
+	public float m_MaxLifeTime = 2f;      
+	SphereCollider theCollider;
+	// Use this for initialization
+	void Start () {
+		theCollider = GetComponent<SphereCollider> ();
+		Destroy(gameObject, m_MaxLifeTime);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+    {
+		Debug.Log("colision:"+other.name);
+        theCollider.isTrigger = false;
+		Destroy (gameObject, 0.3f);
+    }
+
+}
